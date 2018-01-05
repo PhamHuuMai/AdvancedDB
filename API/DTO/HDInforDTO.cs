682fe9361847988d9e726c5437bc18de9ac09ed2
PhamHuuMai <huumaipham1996@gmail.com>
2018-01-05 09:15:32 +0700

    sssssssssssss

--- API/DTO/HDInforDTO.cs
+++ API/DTO/HDInforDTO.cs
@@ -7,6 +7,89 @@ namespace API.DTO
 {
     public class HDInforDTO
     {
-        
+        private int id;
+        private String tenHd;
+        private int idKhoa;
+        private String tenKhoa;
+        private DateTime ngayTL;
+        private int status;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string TenHd
+        {
+            get
+            {
+                return tenHd;
+            }
+
+            set
+            {
+                tenHd = value;
+            }
+        }
+
+        public int IdKhoa
+        {
+            get
+            {
+                return idKhoa;
+            }
+
+            set
+            {
+                idKhoa = value;
+            }
+        }
+
+        public string TenKhoa
+        {
+            get
+            {
+                return tenKhoa;
+            }
+
+            set
+            {
+                tenKhoa = value;
+            }
+        }
+
+        public DateTime NgayTL
+        {
+            get
+            {
+                return ngayTL;
+            }
+
+            set
+            {
+                ngayTL = value;
+            }
+        }
+
+        public int Status
+        {
+            get
+            {
+                return status;
+            }
+
+            set
+            {
+                status = value;
+            }
+        }
     }
 }
