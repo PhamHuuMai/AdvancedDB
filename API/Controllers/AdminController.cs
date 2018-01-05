ea4b8048936939a81f7faa963f16966d42e340dc
PhamHuuMai <huumaipham1996@gmail.com>
2018-01-05 11:11:25 +0700

    gfgjhjlk'

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using API.ResponseDTO;
+using API.Service;
+using API.Service.ServiceImpl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,56 @@ namespace API.Controllers
 {
     public class AdminController : ApiController
     {
+
+        ProtectProjectService protectProjectService = new ProtectProjectImpl();
+
+        [HttpGet]
+        [Route("admin/getallhd")]
+        public IHttpActionResult GetAllHd()
+        {
+            Response response = new Response();
+            try
+            {
+                response.Data = protectProjectService.getAllHD();
+                response.Code = 0;
+                response.Msg = "OK OK";
+            }
+            catch (Exception e)
+            {
+                response.Code = 1;
+                response.Msg = e.Message;
+            }
+            return Ok<Response>(response);
+        }
+
+        [HttpGet]
+        [Route("admin/gethddetail/{id}")]
+        public IHttpActionResult GetHdDetail(int? id)
+        {
+            Response response = new Response();
+            try
+            {
+                // validate  
+                if (id == null)
+                {
+                    response.Code = 1;
+                    response.Msg = "Param invalid";
+                } 
+
+                response.Data = new HDDetailResponse {
+                    HDInfor = protectProjectService.getInfoHD(id.Value),
+                    HDDetai = protectProjectService.getDetailHD(id.Value)
+                };
+                response.Code = 0;
+                response.Msg = "OK OK";
+            }
+            catch (Exception e)
+            {
+                response.Code = 1;
+                response.Msg = e.Message;
+            }
+            return Ok<Response>(response);
+        }
+
     }
 }
---
+++ API/ResponseDTO/HDDetailResponse.cs
@@ -0,0 +1,40 @@
+﻿using API.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.ResponseDTO
+{
+    public class HDDetailResponse
+    {
+        private HDInforDTO hDInfor;
+        private List<HDDetailDTO> hDDetai;
+
+        public HDInforDTO HDInfor
+        {
+            get
+            {
+                return hDInfor;
+            }
+
+            set
+            {
+                hDInfor = value;
+            }
+        }
+
+        public List<HDDetailDTO> HDDetai
+        {
+            get
+            {
+                return hDDetai;
+            }
+
+            set
+            {
+                hDDetai = value;
+            }
+        }
+    }
+}
